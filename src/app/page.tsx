8654c4605e8a7e0c546e152b7fe8c43b7ecabb37
João Victor Mazetti Borges <jvmazettib@gmail.com>
2024-09-19 17:11:54 -0300

    consumindo api de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import styled from "styled-components";
+import { ProductType } from "@/types/ProductType";
 
 const Container = styled.div`
   max-width: 1280px;
   margin: 0 auto;
   border: 1px solid white;
   color: white;
+  margin-top: 20px;
   padding-top: 32px;
   padding-left: 32px;
   padding-right: 32px;
@@ -14,18 +16,29 @@ const ListagemDeProdutos = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 40px;
+
 `
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    throw new Error('failed to find Data')
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products)
   return (
     <>
       <Container>
         <ListagemDeProdutos>
-          <div>prod 1</div>
-          <div>prod 1</div>
-          <div>prod 1</div>
-          <div>prod 1</div>
-          <div>prod 1</div>
+          {products.map((product: ProductType) => (
+            <div key={product.id}>
+              {product.title}
+            </div>
+          ))}
         </ListagemDeProdutos>
       </Container>
     </>
